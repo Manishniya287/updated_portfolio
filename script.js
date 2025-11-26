6128be3a1ebd75bdb6cc26d913e635ab072acfb4
MANISH KUMAR <wanshkumarmanish@gmail.com>
2025-11-26 19:02:04 +0530

    updated portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",{
+    strings:["Python (Pandas, NumPy, Matplotlib, Seaborn)","Data Cleaning & Preprocessing",
+        "Data Visualization (Power BI, Tableau)", "Microsoft Excel", "Statistical Analysis"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
